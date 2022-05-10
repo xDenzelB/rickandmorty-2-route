9de22df7763cdfd35c35c4b8f24dc96a2a2d41be
Denzel <dbartolaba123@gmail.com>
2022-05-10 11:06:30 -0700

    created a details portion for nested route to render

---
+++ src/views/CharacterDetail.jsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+import { Link } from 'react-router-dom';
+import { useParams } from "react-router-dom";
+
+export default function CharacterDetail() {
+  const { id } = useParams();
+  const [character, setCharacters] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function getCharacter() {
+      const res = await fetch(`https://rickandmortyapi.com/api/character/${id}`);
+      const data = await res.json();
+      setCharacters(data);
+      setLoading(false);
+    }
+    getCharacter();
+  }, [])
+
+  return (
+    <div>
+      <h2>{character.name}</h2>
+      <img alt="image" src={character.image} />
+      <p>Species: {character.species}</p>
+      <p>Gender: {character.gender}</p>
+      <p>Status: {character.status}</p>
+    </div>
+  )
+}
